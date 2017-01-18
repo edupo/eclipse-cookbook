3977b66e69330eb3bee64bb78901716ac3999299
Eduardo Lezcano <eduardo.lezcano@be.atlascopco.com>
2017-01-18 11:41:18 +0100

    Added support list for supermarket

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,9 @@ description 'Installs/Configures eclipse'
 long_description 'Installs/Configures Eclipse and/or its plugins (not yet).'
 version '0.1.0'
 
+%w(ubuntu).each do |os|
+  supports os
+end
+
 issues_url 'https://github.com/edupo/eclipse-cookbook/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/edupo/eclipse-cookbook' if respond_to?(:source_url)
